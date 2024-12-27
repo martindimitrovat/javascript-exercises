c1d3e9ac7e3f3011f7d2e957b86066aeda5ac1f0
martindimitrovat <185600172+martindimitrovat@users.noreply.github.com>
2024-12-27 16:11:30 +0100

    Finish set up, exercise 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(stringToBeRepeated, numberOfRepetitions) {
+    if (numberOfRepetitions < 0) return 'ERROR';
+    let repeatedString = '';
+    for (let i = 0; i < numberOfRepetitions; i++) {
+        repeatedString += stringToBeRepeated; 
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
